65d3ff68dad5f755843aec1c4042ae391c07e0a6
rajeshbajya1993 <40539653+rajeshbajya1993@users.noreply.github.com>
2018-08-18 11:18:02 +0530

    ttttttt

--- src/ds/array/GenericArray.java
+++ src/ds/array/GenericArray.java
@@ -5,6 +5,7 @@ public class GenericArray {
 	public static void main(String[] args) {
 		System.out.println("Hello rajesh");
         System.out.println("How are you");
+        System.out.println("kem cho");
 	}
 
 }
