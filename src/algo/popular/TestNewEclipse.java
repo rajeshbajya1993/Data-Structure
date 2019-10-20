ee058e1b1bb491fae32290ac3b29cd978ee2f90e
rajeshbajya1993 <40539653+rajeshbajya1993@users.noreply.github.com>
2019-10-20 13:32:57 +0530

    ty

--- src/algo/popular/TestNewEclipse.java
+++ src/algo/popular/TestNewEclipse.java
@@ -1,9 +1,15 @@
 package algo.popular;
 
+import java.util.HashSet;
+
 public class TestNewEclipse {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Rajesh");
+		
+		HashSet<Integer> hash = new HashSet<Integer>();
+		hash.add(2);
+		
 
 	}
 
